dcd7367ce13f92122795cf30d886eb497028660f
Alora Boone <39552213+Alorafae@users.noreply.github.com>
2019-05-06 13:57:51 -0700

    Initial Commit

---
+++ MemoryManager/MemManager.cpp
@@ -0,0 +1,4 @@
+
+#include "MemManager.h"
+
+
---
+++ MemoryManager/MemManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+/*
+pagesize will be objects/page
+64 byte align everything
+maybe add threading
+*/
+
+typedef class MemManager
+{
+  public:
+    MemManager();
+    ~MemManager();
+
+    bool AddObjType();
+
+  private:
+
+
+};
+
+typedef class ObjManager
+{
+  public:
+    ObjManager();
+    ~ObjManager();
+
+  private:
+
+};
